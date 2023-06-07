bcbc002f7dac46077982bd561ac21770f4733eb0
Maria Lapshina <mariaspiiish@gmail.com>
2023-06-07 20:12:28 +0400

    test

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// import i18next from '../node_modules/i18next/dist/esm/i18next.bundled.js';
+// import translationEN from "./locales/en/translation.json";
+// import translationRU from "./locales/ru/translation.json";
+// const resources = {
+//  en: {
+//    translation: translationEN,
+//  },
+//  ru: {
+//    translation: translationRU,
+//  },
+// };
+
+// i18next.init({
+//   lng: 'ru', // if you're using a language detector, do not define the lng option
+//   debug: true,
+//   resources
+// });
+// // initialized and ready to go!
+// // i18next is already initialized, because the translation resources where passed via init function
+
 const linkRu = document.getElementById('linkRu');
 const linkEn = document.getElementById('linkEn');
 
