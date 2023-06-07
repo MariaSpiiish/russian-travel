a41dd480ac5238967261641b5bca58cef4ea24e2
Maria Lapshina <mariaspiiish@gmail.com>
2023-06-07 20:03:22 +0400

    test

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 const linkRu = document.getElementById('linkRu');
 const linkEn = document.getElementById('linkEn');
-console.log(location.href.split('?')[1])
 
 if( location.href.split('?')[1] ==  'lng=en') {
     linkEn.classList.add('header__lang-link_active');
 } else {
     linkRu.classList.add('header__lang-link_active');
 }
-
-// linkRu.addEventListener('click', () => {
-//     linkRu.classList.add('header__lang-link_active');
-//     linkEn.classList.remove('header__lang-link_active');
-// })
-
-// linkEn.addEventListener('click', () => {
-//     linkEn.classList.add('header__lang-link_active');
-//     linkRu.classList.remove('header__lang-link_active');
-// })
